7aed2654da24454d87db83ff0d543dcd9cc4d5a0
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-03-18 10:26:43 +0700

    Add files via upload

--- git.py
+++ git.py
@@ -1,10 +1,13 @@
 #!usr/bin/python3.7
 #Author: KANG-NEWBIE
-#Team: 407AEX
 #Contact: t.me/kang_nuubi
-
-from requests import get, post
-import json, os, subprocess as sp
+try:
+	from requests import get, post
+	import json, os, subprocess as sp, sys
+except Exception as ff:
+	exit("[ModuleErr] %s"%(ff))
+if sys.version[0] in '2':
+   exit("[sorry] Kamu menggunakan python2 silahkan install python3")
 os.system('clear')
 print("""
   __ ___ ___    _     _        _ 
@@ -13,6 +16,8 @@ print("""
                                  """)
 try:
 	u=input("[!] input username github: ")
+	if u == '':
+		exit("[?] anda waras")
 	print()
 	re=get('https://api.github.com/users/'+u+'/repos')
 	a=json.loads(re.text)
@@ -23,20 +28,26 @@ items = []
 try:
 	for item in a:
 	    items.append(item['name'])
-except TypeError: exit("[!] username tidak ada")
+except TypeError: exit("[user:Not Found] username tidak ada")
 
 s=len(items)
-if s == 0:
-	exit("[!] username tersebut tidak memiliki repostory apapun")
+if s <= 0:
+	exit("[repo:Not Found] username tersebut tidak memiliki repostory apapun")
+elif s > 0:
+	print("[!] found:",s,"repostory")
+
 c=int(1)
 while c < s:
-	for i in range(len(items)):
-		print('\t['+str(c)+"] Install",items[i])
+	for i in range(s):
+		print("\t["+str(c)+"] Clone",items[i])
 		c+=1
 try:
-	asu=int(input("\n[?] install yang mana: "))
+	asu=int(input("\n[?] Clone yang mana: "))
+	if asu == 0:
+		exit("[?] anda waras")
 	os.system('git clone https://github.com/'+u+'/'+items[asu-1])
 	sp.call('mv '+str(items[asu-1])+' ..',shell=True,stdout=sp.DEVNULL, stderr=sp.STDOUT)
 	print("[!] success installed",items[asu-1])
 	print("[!] check the folder type 'cd'")
+except KeyboardInterrupt: exit("[!] key interrupt: exit")
 except Exception as F: exit("[?: anda waras] %s"%(F))
